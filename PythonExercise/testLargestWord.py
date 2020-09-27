44f9f5d0448cb1436be9744b0be0f0066280828d
Felipe Jiménez <felipejbq@hotmail.com>
2020-09-27 14:38:28 -0500

    Update Readme to include python excercise instructions

--- PythonExercise/testLargestWord.py
+++ PythonExercise/testLargestWord.py
@@ -3,26 +3,33 @@ import largestWord
 
 class TestLargestWord(unittest.TestCase):
 
+  #Test if success cases of getWordsFromFile function work propperly
   def testSuccessGetWordsFromFile(self):
     self.assertEqual(largestWord.getWordsFromFile('./testData/testFile.txt'), ['hello\n', 'from\n', 'the\n', 'other\n', 'side'])
 
+  #Test if error cases of getWordsFromFile function work propperly
   def testErrorGetWordsFromFile(self):
     with self.assertRaises(TypeError):
       largestWord.getWordsFromFile(12345)
 
+  #Test if success cases of reverseWord function work propperly
   def testSuccessReverseWord(self):
     self.assertEqual(largestWord.reverseWord('electron'), 'nortcele')
 
+  #Test if failure cases of reverseWord function work propperly
   def testFailureReverseWord(self):
     self.assertNotEqual(largestWord.reverseWord('electron'), 'electron')
 
+  #Test if error cases of reverseWord function work propperly
   def testErrorReverseWord(self):
     with self.assertRaises(TypeError):
       largestWord.reverseWord(12345)
 
+  #Test if success cases of GetLongestWord function work propperly
   def testSuccessGetLongestWord(self):
     self.assertEqual(largestWord.getLongestWord(['the\n', 'largest\n', 'word\n', 'what\n', 'is']), 'largest\n')
 
+  #Test if failure cases of GetLongestWord function work propperly
   def testFailureGetLongestWord(self):
     self.assertNotEqual(largestWord.getLongestWord(['the\n', 'largest\n', 'word\n', 'what\n', 'development\n', 'is']), 'largest\n')
     
--- README.md
+++ README.md
@@ -2,8 +2,19 @@
 Repository to track the GAP excersises by Felipe Jimenez.
 
 ## Python Largest Word Excercise
+Python version 3 or higher is required to execute this script. <br /><br />
+
+In order to run the largest word exercise, please enter to the `PythonExercise` folder. <br /><br />
+
+To run the script, enter to cmd on current location, and run command `python3 largestWord.py`. The file in which the 
+words are is `wordsFile.txt`, it can be modified or replaced as long as the name and format keeps the same.
+
+To run the script unit tests, enter to cmd on current location, and run command `python3 testLargestWord.py`. These tests 
+are suported by data files located on the `testData` folder, these files should not be modified.
 
 ## Cypress Shopping Excercise
+Node.js version 10 or higher and npm is required to execute this script. <br /><br />
+
 In order to install the framework, please enter to the `ShoppingAutomation` folder, then, run via cmd the command
 `npm install`, this will install all required dependencies. <br /><br />
 
